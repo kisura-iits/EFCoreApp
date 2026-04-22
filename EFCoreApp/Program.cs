992aaf582f8d09e7412ee86cc3103e051e685887
kisura-iits <kisura.waynath@iits.biz>
2026-04-22 15:22:04 +0530

    chapter 1 done

--- EFCoreApp/Program.cs
+++ EFCoreApp/Program.cs
@@ -1,9 +1,37 @@
+using EFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SchoolContext>();
+        // Ensure the database is created and migrations are applied if necessary
+        context.Database.EnsureCreated();
+
+        // Call your custom initializer
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
